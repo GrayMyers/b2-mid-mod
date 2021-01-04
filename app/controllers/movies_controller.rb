806b20b3cebfe0ccaf167a60bc05d6018a619268
Grayson Myers <graymyers10@gmail.com>
2021-01-04 10:39:57 -0700

    Add update method to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,12 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    movie = Movie.find(params[:id])
+    actor = Actor.where(name: params[:name])[0]
+    ActorMovie.create(movie: movie, actor: actor)
+
+    redirect_to("/movies/#{movie.id}")
+  end
 end
