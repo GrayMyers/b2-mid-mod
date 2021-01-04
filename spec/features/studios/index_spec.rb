01376d5b65f5dda0c1ab986e06c9b8fe27b671b2
Grayson Myers <graymyers10@gmail.com>
2021-01-04 09:34:13 -0700

    Fix typo

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -6,9 +6,9 @@ describe "studio index page" do
 
     @studio2 = Studio.create(name: "studio 2", location: "there")
 
-    @movie1 = studio1.movies.create(title: "movie 1", year: "1924", genre: "old movies")
-    @movie2 = studio1.movies.create(title: "movie 2", year: "2020", genre: "horror")
-    @movie3 = studio2.movies.create(title: "movie 3", year: "1880", genre: "not a movie")
+    @movie1 = @studio1.movies.create(title: "movie 1", year: "1924", genre: "old movies")
+    @movie2 = @studio1.movies.create(title: "movie 2", year: "2020", genre: "horror")
+    @movie3 = @studio2.movies.create(title: "movie 3", year: "1880", genre: "not a movie")
 
     @actor1 = Actor.create(name: "actor 1", age: "2")
     @actor2 = Actor.create(name: "actor 2", age: "47")
