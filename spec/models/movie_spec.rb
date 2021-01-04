d7ad2879d2912189e899ceb6206310dc9ad4c33d
Grayson Myers <graymyers10@gmail.com>
2021-01-04 09:39:05 -0700

    Update movie spec to include relationships with actor model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 describe Movie, type: :model do
-  describe "relationships" do 
+  describe "relationships" do
     it {should belong_to :studio}
+    it {should have_many :actors}
+    it {should have_many :actor_movies}
+
   end
 end
