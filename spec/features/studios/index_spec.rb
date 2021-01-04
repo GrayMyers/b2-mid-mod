5e3f323e095512990c67548f235d44e27c4af711
Grayson Myers <graymyers10@gmail.com>
2021-01-04 09:30:06 -0700

    Add test for studio index page

---
+++ spec/features/studios/index_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe "studio index page" do
+  before :each do
+    @studio1 = Studio.create(name: "studio 1", location: "here")
+
+    @studio2 = Studio.create(name: "studio 2", location: "there")
+
+    @movie1 = studio1.movies.create(title: "movie 1", year: "1924", genre: "old movies")
+    @movie2 = studio1.movies.create(title: "movie 2", year: "2020", genre: "horror")
+    @movie3 = studio2.movies.create(title: "movie 3", year: "1880", genre: "not a movie")
+
+    @actor1 = Actor.create(name: "actor 1", age: "2")
+    @actor2 = Actor.create(name: "actor 2", age: "47")
+    @actor3 = Actor.create(name: "actor 3", age: "28")
+
+    ActorMovies.create(movie: @movie1, actor: @actor1)
+    ActorMovies.create(movie: @movie1, actor: @actor2)
+
+    ActorMovies.create(movie: @movie2, actor: @actor2)
+    ActorMovies.create(movie: @movie2, actor: @actor3)
+
+    ActorMovies.create(movie: @movie3, actor: @actor1)
+    ActorMovies.create(movie: @movie3, actor: @actor3)
+  end
+
+  it "shows all studios and has their associated movies under those studios" do
+    visit "/studios/"
+    within(page.find("#s-#{@studio1.id}")) do
+      expect(page).to have_content("movie 1")
+      expect(page).to have_content("movie 2")
+    end
+
+    within(page.find("#s-#{@studio2.id}")) do
+      expect(page).to have_content("movie 3")
+    end
+  end
+end
