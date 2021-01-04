3c053f3883fda167a3c27e104da65006a4486a5c
Grayson Myers <graymyers10@gmail.com>
2021-01-04 12:03:06 -0700

    Finalize work for submission

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ApplicationRecord
   has_many :actor_movies
   has_many :movies, through: :actor_movies
+
+  def associates
+    #not currently working and not clean code, was running out of time and wanted to get it working
+    assoc = movies.collect { |movie| movie.actors }.uniq[0]{ |actor| actor.id}
+  end
 end
